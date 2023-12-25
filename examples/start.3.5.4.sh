5e18846b04e95ad2616344d6d8b0b6add6a7e76d
Dominique Jc Paul <dominiqu-jc@users.sf.net>
2023-12-25 18:31:21 +0100

    missed correction

--- examples/start.3.5.4.sh
+++ examples/start.3.5.4.sh
@@ -3,4 +3,4 @@
 # period: 36 days
 # number of hoses: 12
 # radius: 4 mm
-simulWaterHoseWall --lengthU=0.002 --timeU=10 --width=0.94 --deep=2. --period=3110400 --earthLambda=0.6 --earthVHeatCap=1.8E6 --waterVHeatCap=4.18E6 --hoseNb=12 --hoseRadius=0.004 --hose1Deep=0.100 --hoseInterv=0.164 res.3.5.4
+simulWaterHoseWall --lengthU=0.002 --timeU=10 --width=0.094 --deep=2. --period=3110400 --earthLambda=0.6 --earthVHeatCap=1.8E6 --waterVHeatCap=4.18E6 --hoseNb=12 --hoseRadius=0.004 --hose1Deep=0.100 --hoseInterv=0.164 res.3.5.4
